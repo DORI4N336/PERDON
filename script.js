5162592e58f1bb88c5396d91026248b6eeda0eb1
DORI4N336 <rojasdorian336@gmail.com>
2026-02-04 03:44:17 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const noBtn = document.getElementById('noBtn');
 const siBtn = document.getElementById('siBtn');
 
+
+noBtn.addEventListener('mouseover', function () {
+    const randomX = parseInt(Math.random() * 100);
+    const randomY = parseInt(Math.random() * 100);
+    
+    
     noBtn.style.setProperty('top', randomY + '%');
     noBtn.style.setProperty('left', randomX + '%');
     noBtn.style.setProperty('transform', `translate(-${randomX}%, -${randomY}%)`);
