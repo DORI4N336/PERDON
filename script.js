094173a390e70bbb694721f3e5e0577a84a31368
DORI4N336 <rojasdorian336@gmail.com>
2026-02-04 03:42:00 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const noBtn = document.getElementById('noBtn');
+const siBtn = document.getElementById('siBtn');
+
+    noBtn.style.setProperty('top', randomY + '%');
+    noBtn.style.setProperty('left', randomX + '%');
+    noBtn.style.setProperty('transform', `translate(-${randomX}%, -${randomY}%)`);
+});
+
+siBtn.addEventListener('click', function () {
+    alert('¡Sabía que me perdonarías! ❤️ Gracias.');
