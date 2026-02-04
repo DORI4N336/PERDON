05ca2b573bf4627bb203f9d48663a033bb08cfc9
DORI4N336 <rojasdorian336@gmail.com>
2026-02-04 03:51:53 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const noBtn = document.getElementById('noBtn');
 const siBtn = document.getElementById('siBtn');
+
+// Aquí agregamos el evento 'mouseover' (cuando el mouse pasa por encima)
+noBtn.addEventListener('mouseover', function () {
+    // 1. Calculamos números aleatorios de 0 a 100
+    const randomX = parseInt(Math.random() * 100);
+    const randomY = parseInt(Math.random() * 100);
     
+    // 2. Aplicamos esos números a la posición del botón
     noBtn.style.setProperty('top', randomY + '%');
     noBtn.style.setProperty('left', randomX + '%');
+    
+    // 3. Esto asegura que el botón no se salga de la pantalla
     noBtn.style.setProperty('transform', `translate(-${randomX}%, -${randomY}%)`);
+});
 
 siBtn.addEventListener('click', function () {
     alert('¡Sabía que me perdonarías! ❤️ Gracias.');
+});
