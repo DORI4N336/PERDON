8ea094b85a6f3d5d09ff05afc33b5d9953c444ca
DORI4N336 <rojasdorian336@gmail.com>
2026-02-04 03:45:39 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 const noBtn = document.getElementById('noBtn');
 const siBtn = document.getElementById('siBtn');
-
-
-noBtn.addEventListener('mouseover', function () {
-    const randomX = parseInt(Math.random() * 100);
-    const randomY = parseInt(Math.random() * 100);
-    
     
     noBtn.style.setProperty('top', randomY + '%');
     noBtn.style.setProperty('left', randomX + '%');
     noBtn.style.setProperty('transform', `translate(-${randomX}%, -${randomY}%)`);
-});
 
 siBtn.addEventListener('click', function () {
     alert('¡Sabía que me perdonarías! ❤️ Gracias.');
